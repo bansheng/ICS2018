2f86054c2436e5d68f136aa999b88e931e439fc0
tracer-ics2018 <tracer@njuics.org>
2018-12-10 19:11:59 -0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:11:59 up 1 day, 15:13,  5 users,  load average: 0.03, 0.12, 0.09 95153f84a330a997974c3d896fd15fe6b0efcce0

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -63,9 +63,9 @@ static int cmd_si(char *args) {
 // 打印寄存器内容的子函数
 static void print_regster(int size, int index) {
 	switch(size) {
-		case 1: printf("%s\t0x%02X\t\t\t%d\n", reg_name(index, 1), reg_b(index), reg_b(index)); break;
-		case 2: printf("%s\t0x%04X\t\t%d\n", reg_name(index, 2), reg_w(index), reg_w(index)); break;
-		case 4: printf("%s\t0x%08X\t%d\n", reg_name(index, 4), reg_l(index), reg_l(index)); break;
+		case 1: printf("%s\t%#04X\t\t%d\n", reg_name(index, 1), reg_b(index), reg_b(index)); break;
+		case 2: printf("%s\t%#06X\t\t%d\n", reg_name(index, 2), reg_w(index), reg_w(index)); break;
+		case 4: printf("%s\t%#010X\t%d\n", reg_name(index, 4), reg_l(index), reg_l(index)); break;
 		//case type_eip: printf("EIP/t%-08x/t%d\n", cpu.eip, cpu.eip); 
 	}
 	
@@ -113,7 +113,7 @@ static int cmd_scan(char *args) {
 		printf("Please input the size and address to scan\n");
 	}
 	for(int i=0; i<size; i++)
-		printf("0x%08X\t", vaddr_read(address + i * 4, 4));
+		printf("%#010X\t", vaddr_read(address + i * 4, 4));
 	printf("\n");
 	return 0;
 }
@@ -131,7 +131,7 @@ static struct {
   /* TODO: Add more commands */
   { "si", "Single instruction execution", cmd_si },
   { "info", "Print the regsters or watchpoints", cmd_info },
-  { "scan", "Scan the memory to find the value", cmd_scan },//扫描内存
+  { "x", "Scan the memory to find the value", cmd_scan },//扫描内存
 };
 // 定义最大支持的命令表下标
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
