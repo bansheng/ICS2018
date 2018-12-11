93a547df1ee561daba0b361626c8c8a5f2615311
tracer-ics2018 <tracer@njuics.org>
2018-12-10 19:05:33 -0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:05:33 up 1 day, 15:07,  5 users,  load average: 0.44, 0.22, 0.12 c3067a66626759a6e0962af1f547e97930c62fea

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -63,9 +63,9 @@ static int cmd_si(char *args) {
 // 打印寄存器内容的子函数
 static void print_regster(int size, int index) {
 	switch(size) {
-		case 1: printf("%s\t%-2x\t\t\t%d\n", reg_name(index, 1), reg_b(index), reg_b(index)); break;
-		case 2: printf("%s\t%-4x\t\t%d\n", reg_name(index, 2), reg_w(index), reg_w(index)); break;
-		case 4: printf("%s\t%-8x\t%d\n", reg_name(index, 4), reg_l(index), reg_l(index)); break;
+		case 1: printf("%s\t%02X\t\t\t%d\n", reg_name(index, 1), reg_b(index), reg_b(index)); break;
+		case 2: printf("%s\t%04X\t\t%d\n", reg_name(index, 2), reg_w(index), reg_w(index)); break;
+		case 4: printf("%s\t%08X\t%d\n", reg_name(index, 4), reg_l(index), reg_l(index)); break;
 		//case type_eip: printf("EIP/t%-08x/t%d\n", cpu.eip, cpu.eip); 
 	}
 	
@@ -113,7 +113,7 @@ static int cmd_scan(char *args) {
 		printf("Please input the size and address to scan\n");
 	}
 	for(int i=0; i<size; i++)
-		printf("%-8x\t", vaddr_read(address + i * 4, 4));
+		printf("%08X\t", vaddr_read(address + i * 4, 4));
 	printf("\n");
 	return 0;
 }
