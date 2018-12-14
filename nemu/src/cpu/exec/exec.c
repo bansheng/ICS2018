a7055b41f3f5dc596ca0c8aeb9060856b83cd5dd
tracer-ics2018 <tracer@njuics.org>
2018-12-14 11:02:45 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:02:45 up 3 days,  3:35,  5 users,  load average: 0.05, 0.12, 0.11 831aa280a2f2b0c6a5cf7a23fd2b1d07108aa92

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -99,7 +99,7 @@ opcode_entry opcode_table [512] = {
   /* 0x5c */	IDEX(r,pop), IDEX(r,pop), IDEX(r,pop), IDEX(r, pop),
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
-  /* 0x68 */	EMPTY, EMPTY, IDEXW(G2E,push,1), EMPTY,
+  /* 0x68 */	EMPTY, EMPTY, IDEXW(SI,push,1), EMPTY,
   /* 0x6c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x74 */	EMPTY, EMPTY, EMPTY, EMPTY,
