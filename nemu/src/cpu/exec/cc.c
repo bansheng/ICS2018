3d4c5ddecf47f48e80e28e0d1e025b0416b37c77
tracer-ics2018 <tracer@njuics.org>
2018-12-15 23:01:42 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:01:42 up 3 days, 10:15,  6 users,  load average: 0.37, 0.21, 0.18 9f68330fc9c2cfe4e4b4305420229e763c7aaf0d

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -3,30 +3,47 @@
 /* Condition Code */
 
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
-  bool invert = subcode & 0x1;
-  enum {
-    CC_O, CC_NO, CC_B,  CC_NB,
-    CC_E, CC_NE, CC_BE, CC_NBE,
-    CC_S, CC_NS, CC_P,  CC_NP,
-    CC_L, CC_NL, CC_LE, CC_NLE
-  };
+	bool invert = subcode & 0x1;
+	enum {
+		CC_O, CC_NO, CC_B,  CC_NB,
+		CC_E, CC_NE, CC_BE, CC_NBE,
+		CC_S, CC_NS, CC_P,  CC_NP,
+		CC_L, CC_NL, CC_LE, CC_NLE
+	};
 
-  // TODO: Query EFLAGS to determine whether the condition code is satisfied.
-  // dest <- ( cc is satisfied ? 1 : 0)
-  switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
-    default: panic("should not reach here");
-    case CC_P: panic("n86 does not have PF");
-  }
+	// TODO: Query EFLAGS to determine whether the condition code is satisfied.
+	// dest <- ( cc is satisfied ? 1 : 0)
+	switch (subcode & 0xe) {
+		case CC_O:  //0
+			*dest = cpu.eflags.OF;
+			break;
+		case CC_B:  //2
+			*dest = cpu.eflags.CF;
+			break;
+		case CC_E:  //4
+			//printf("i am here\n");
+			//printf("zf = %d\n", cpu.eflags.ZF); 
+			*dest = cpu.eflags.ZF;
+			break;
+		case CC_BE: //6
+			//printf("switch = %x\n", subcode&0xe); 
+			*dest = ((cpu.eflags.CF) || (cpu.eflags.ZF));
+			//printf("dest = %d\n", *dest);
+			break;
+		case CC_S:  //8
+			*dest = cpu.eflags.SF;
+			break;
+		case CC_L:  //12 c
+			*dest = (cpu.eflags.SF != cpu.eflags.OF);
+			break;
+		case CC_LE: //14 e
+			*dest = ((cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF));
+			break;
+		default: panic("should not reach here");
+		case CC_P: panic("n86 does not have PF");
+	}
 
-  if (invert) {
-    rtl_xori(dest, dest, 0x1);
-  }
+	if (invert) {
+		rtl_xori(dest, dest, 0x1);
+	}
 }
