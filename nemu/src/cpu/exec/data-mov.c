f54af5e09eefc6e3ea27a4de983464a2b4d6f333
tracer-ics2018 <tracer@njuics.org>
2018-12-15 23:17:55 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:17:55 up 3 days, 10:32,  6 users,  load average: 0.22, 0.21, 0.19 ba6db49f71be1f1e38924abcd41480c7e10783a5

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -19,9 +19,9 @@ make_EHelper(push) {
 
 make_EHelper(pop) {
 	// TODO();
-	rtl_pop(&at);
+	rtl_pop(&t0);
 	if(id_dest->width == 1){
-		uint8_t utemp = at;
+		uint8_t utemp = t0;
 		int8_t temp = utemp; 
 		id_dest->val = temp;
 	}
@@ -46,8 +46,8 @@ make_EHelper(popa) {
 make_EHelper(leave) {
 	// TODO();
 	reg_l(4) = reg_l(5);
-	rtl_pop(&at);
-	reg_w(5) = at;
+	rtl_pop(&t0);
+	reg_w(5) = t0;
 	
 	print_asm("leave");
 }
@@ -64,30 +64,30 @@ make_EHelper(cltd) {
 }
 
 make_EHelper(cwtl) {
-  if (decoding.is_operand_size_16) {
-    TODO();
-  }
-  else {
-    TODO();
-  }
+	if (decoding.is_operand_size_16) {
+		TODO();
+	}
+	else {
+		TODO();
+	}
 
-  print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
+	print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
 make_EHelper(movsx) {
-  id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
-  rtl_sext(&t0, &id_src->val, id_src->width);
-  operand_write(id_dest, &t0);
-  print_asm_template2(movsx);
+	id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
+	rtl_sext(&t0, &id_src->val, id_src->width);
+	operand_write(id_dest, &t0);
+	print_asm_template2(movsx);
 }
 
 make_EHelper(movzx) {
-  id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
-  operand_write(id_dest, &id_src->val);
-  print_asm_template2(movzx);
+	id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
+	operand_write(id_dest, &id_src->val);
+	print_asm_template2(movzx);
 }
 
 make_EHelper(lea) {
-  operand_write(id_dest, &id_src->addr);
-  print_asm_template2(lea);
+	operand_write(id_dest, &id_src->addr);
+	print_asm_template2(lea);
 }
