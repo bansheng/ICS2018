407ac94013af9930f909e1477a6e8e7442cb578a
tracer-ics2018 <tracer@njuics.org>
2018-12-16 01:34:26 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  01:34:26 up 3 days, 12:48,  6 users,  load average: 0.22, 0.19, 0.15 fae616b0d49beaefd83563a7bb09c78e1784a8d4

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -26,6 +26,7 @@ static inline void idex(vaddr_t *eip, opcode_entry *e) {
   /* eip is pointing to the byte next to opcode */
   if (e->decode)
     e->decode(eip);
+  printf("0x%x\n", decoding.opcode);
   e->execute(eip);
 }
 
