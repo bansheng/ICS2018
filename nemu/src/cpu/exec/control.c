b3806e468bea2ed326f890c442529f2f16806138
tracer-ics2018 <tracer@njuics.org>
2018-12-16 00:16:06 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:16:06 up 3 days, 11:30,  6 users,  load average: 0.07, 0.19, 0.18 8a6c7ab0f161e52414e413396ca12cba8cda8fa7

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -43,7 +43,10 @@ make_EHelper(ret) {
 }
 
 make_EHelper(call_rm) {
-	TODO();
+	// TODO();
+	rtl_push(&decoding.seq_eip);
+	decoding.jmp_eip = id_dest->val;
+	decoding.is_jmp = 1;
 
 	print_asm("call *%s", id_dest->str);
 }
