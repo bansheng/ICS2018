57e9dc729d8feb974584c054118d45afb7f71193
tracer-ics2018 <tracer@njuics.org>
2018-12-16 19:42:26 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:42:26 up 3 days, 23:51,  5 users,  load average: 0.08, 0.14, 0.11 e48e7b994185c853598164e438cc5cf6df60eee9

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -10,41 +10,85 @@ size_t strlen(const char *s) {
 
 char *strcpy(char* dst,const char* src) {
 	// 注意前后覆盖的问题
-	char ch[strlen(src)];
-	char *head = ch;
-	while((*(head++) = *(src++))) ;
-	head = ch;
-	char *result = dst;
-	while((*(dst++) = *(head))) ;
-	return result;
+	return strncpy(dst, src, strlen(src));
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-	return NULL;
+	size_t size_src = strlen(src);
+	if( n > size_src) return strncpy(dst, src, size_src);
+	else // n <= size_src
+	{
+		// 注意前后覆盖的问题
+		char ch[n+1];
+		char *head = ch;
+		size_t i = 0;
+		while( i < n ) {
+			*(head+i) = *(src+i);
+			i++;
+		} 
+		ch[n] = '\0';
+		head = ch;
+		char *result = dst;
+		while((*(dst++) = *(head++))) ;
+		return result;
+	}
 }
 
 char* strcat(char* dst, const char* src) {
-	return NULL;
+	char *result = dst;
+	while(*dst) dst++;
+	strcpy(dst, src);
+	return result;
 }
 
 int strcmp(const char* s1, const char* s2) {
-	return 0;
+	int a = 0;
+	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2) {
+		s1++;
+		s2++;
+	}
+	return a;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-	return 0;
+	int a = 0;
+	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2 && n) {
+		s1++;
+		s2++;
+		n--;
+	}
+	return a;
 }
 
 void* memset(void* v,int c,size_t n) {
-	return NULL;
+	void* ret = v;
+    while(n--)
+    {
+        *(char*)v = (char)c;
+        v = (char*)v + 1; //移动一个字节
+    }
+    return ret;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-	return NULL;
+	void *ret = out; //可能会出现覆盖的问题
+	while(n--)
+	{
+		*((char*)out + n -1) = *((char*)in + n -1);
+	}
+	return ret;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-	return 0;
+	const char *begin1 = (const char *)s1;
+	const char *begin2 = (const char *)s2;
+	int a = 0;
+	while( (a = (*begin1 - *begin2)) == 0 && n) {
+		begin1++;
+		begin2++;
+		n--;
+	}
+	return a;
 }
 
 #endif
