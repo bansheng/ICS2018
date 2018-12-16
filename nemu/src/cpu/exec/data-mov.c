1c476d978a9131d3cb162ec1109e9e207da16392
tracer-ics2018 <tracer@njuics.org>
2018-12-16 15:40:21 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:40:21 up 3 days, 19:49,  6 users,  load average: 0.31, 0.26, 0.14 4a3505d24da921d8cdac9037d91fa7cbc39cd5e

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -21,7 +21,7 @@ make_EHelper(pop) {
 	// TODO();
 	rtl_pop(&t0);
 	
-	printf("%d width %d\n",id_dest->type, id_dest->width);
+	
 	if(id_dest->width == 1){
 		uint8_t utemp = t0;
 		int8_t temp = utemp; 
@@ -30,7 +30,8 @@ make_EHelper(pop) {
 	else 
 	  	id_dest->val = t0;
 	// 写入
-	operand_write(id_dest, &id_src->val);
+	printf("%d width %d  0x%x\n",id_dest->type, id_dest->width, id_dest->val);
+	operand_write(id_dest, &id_dest->val);
 	
 	print_asm_template1(pop);
 }
