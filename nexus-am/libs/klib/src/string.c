7c3ffdaced582fe9dd79b26ee3fc47e501f48164
tracer-ics2018 <tracer@njuics.org>
2018-12-16 18:44:03 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:44:03 up 3 days, 22:53,  6 users,  load average: 0.18, 0.12, 0.09 dca4de2272795e8432d09133abcb2ba886bae22a

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -9,35 +9,42 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+	// 注意前后覆盖的问题
+	char ch[strlen(src)];
+	char *head = ch;
+	while((*(head++) = *(src++))) ;
+	head = ch;
+	char *result = dst;
+	while((*(dst++) = *(head))) ;
+	return result;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+	return NULL;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+	return NULL;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+	return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+	return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+	return NULL;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+	return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+	return 0;
 }
 
 #endif
