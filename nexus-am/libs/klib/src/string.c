8bd4a821646ea18080afe9984d10dbb04c08c159
tracer-ics2018 <tracer@njuics.org>
2018-12-16 20:54:52 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:54:52 up 4 days,  1:03,  5 users,  load average: 0.16, 0.03, 0.01 b3f701e5ce488a3fba14f07f426eeefad8a068f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -2,105 +2,90 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) { // pass
-	size_t size = 0;
-	while(*(s++)) size++;
-	// printf("%s %d\n", s, (int)size);
-	return size;
+size_t strlen(const char *s) {
+  int len = 0;
+  while(*s++ != '\0')
+        len++;
+  return len;
 }
 
-char *strcpy(char* dst,const char* src) { //pass
-	// 注意前后覆盖的问题
-	return strncpy(dst, src, strlen(src));
+char *strcpy(char* dst,const char* src) {
+  char *srcdst = dst;
+  while((*dst++ = *src++) != '\0');
+  return srcdst;
 }
 
-char* strncpy(char* dst, const char* src, size_t n) { //pass
-	size_t size_src = strlen(src);
-	if( n > size_src) return strncpy(dst, src, size_src);
-	else // n <= size_src
-	{
-		// printf("strncpy1 %s %s %d\n", dst, src, (int)n);
-		// 注意前后覆盖的问题
-		char ch[n+1];
-		char *head = ch;
-		size_t i = 0;
-		while( i < n ) {
-			*(head+i) = *(src+i);
-			i++;
-		} 
-		ch[n] = '\0';
-		head = ch;
-		char *result = dst;
-		while((*(dst++) = *(head++))) ;
-		// printf("strncpy2 %s\n", dst);
-		return result;
-	}
+char* strncpy(char* dst, const char* src, size_t n) {
+  char *srcdst = dst;
+  while(*src != '\0' && n--)
+     *dst++ = *src++;
+  *dst = '\0';
+  return srcdst;
 }
 
-char* strcat(char* dst, const char* src) { //pass
-	// printf("strcat1 %s %s\n", dst, src);
-	char *result = dst;
-	while(*dst) dst++;
-	strcpy(dst, src);
-	// printf("strcat2 %s\n", dst);
-	return result;
+char* strcat(char* dst, const char* src) {
+  char *temp = dst;
+  while(*dst != '\0')
+     dst++;
+  while((*dst++ = *src++) != '\0');
+  return temp;
 }
 
-int strcmp(const char* s1, const char* s2) { //pass
-	// printf("strcmp1 %s %s\n", s1, s2);
-	int a = 0;
-	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2) {
-		s1++;
-		s2++;
-	}
-	// printf("strcmp2 %d\n", a);
-	return a;
+int strcmp(const char* s1, const char* s2) {
+  assert((s1 != NULL) && (s2 != NULL));
+  while( *s1 && *s2 && *s1==*s2){
+     ++s1;
+     ++s2;
+  }
+  return (*s1 - *s2);
 }
 
-int strncmp(const char* s1, const char* s2, size_t n) { //pass
-	// printf("strncmp1 %s %s %d\n", s1, s2, (int)n);
-	int a = 0;
-	while( (a = (*s1 - *s2)) == 0 && *s1 && *s2 && n) {
-		s1++;
-		s2++;
-		n--;
-	}
-	// printf("strncmp2 %d\n", a);
-	return a;
+int strncmp(const char* s1, const char* s2, size_t n) {
+  assert((s1 != NULL) && (s2 != NULL));
+  while( *s1 && *s2 && *s1==*s2 && n--){
+     ++s1;
+     ++s2;
+  }
+  return (*s1 - *s2);
 }
 
 void* memset(void* v,int c,size_t n) {
-	printf("11111111111111111111111111\n");
-	void* ret = v;
-    while(n--)
-    {
-        *(char*)v = (char)c;
-        v = (char*)v + 1; //移动一个字节
-    }
-    return ret;
-	return NULL;
+  char * pvTo=(char *)v;
+  assert(v != NULL);
+  while(n-- >0)
+    *pvTo++ = (char)c;
+  *pvTo = '\0';
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-	void *ret = out; //可能会出现覆盖的问题
-	char *begin1 = (char *)out;
-	const char *begin2 = (const char *)in;
-	while(n--)
-	{
-		*(begin1+n-1) = *(begin2+n-1);
-	}
-	return ret;
+  char * pbIn = (char *)in;
+  char * pbOut = (char *)out;
+  assert(pbIn != NULL && pbOut != NULL);//不能存在空指针
+  assert(pbIn >= pbOut+n || pbOut >= pbIn+n);//防止内存重叠
+  while(n-- >0)
+     *pbOut++ = *pbIn++;
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-	const char *begin1 = (const char *)s1;
-	const char *begin2 = (const char *)s2;
-	int a = 0;
-	while( (a = (*begin1 - *begin2)) == 0 && n--) {
-		begin1++;
-		begin2++;
-	}
-	return a;
+  /*assert((s1 != NULL) && (s2 != NULL));
+  char *tmp1 = (char *)s1;
+  char *tmp2 = (char *)s2;
+  while(n--){
+     while(*tmp1 == *tmp2){
+        if(*tmp1 == '\0')
+            return 0;
+        tmp1++;
+        tmp2++;
+     }
+  }
+  if(*tmp1 > *tmp2)
+       return 1;
+  if(*tmp1 < *tmp2)
+       return -1;*/
+  return 0;
 }
 
 #endif
+
