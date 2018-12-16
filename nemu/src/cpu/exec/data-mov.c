5b666aa0aee65d46e8b20cd21e83e9b37139d49b
tracer-ics2018 <tracer@njuics.org>
2018-12-16 14:32:14 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:32:14 up 3 days, 18:41,  6 users,  load average: 0.26, 0.09, 0.06 ff8cfcdd41ab8a9a38b759b1757b316dc156d9

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -69,34 +69,28 @@ make_EHelper(leave) {
 	print_asm("leave");
 }
 
+// Convert Signed Long to Signed Double Long
 make_EHelper(cltd) {
 	if (decoding.is_operand_size_16) {
-		rtl_lr(&t0, R_AX, 2);
-		rtl_sext(&t0, &t0, 2);
-		rtl_sari(&t0, &t0, 16);
-		rtl_sr(R_DX, &t0, 2);
-	} else {
-		rtl_lr(&t0, R_EAX, 4);
-		rtl_sari(&t0, &t0, 31);
-		rtl_sari(&t0, &t0, 1);
-		rtl_sr(R_EDX, &t0, 4);
-	}
-
-	print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
-}
-
-make_EHelper(cwtl) {
-	if (decoding.is_operand_size_16) {
-		rtl_lr(&t0, R_AX, 1);
-		rtl_sext(&t0, &t0, 1);
-		rtl_sr(R_AX, &t0, 2);
+		rtl_sext(&t0, &reg_l(R_EAX), 2);
+		rtl_shri(&reg_l(R_EDX), &t0, 16);
 	}
 	else {
-		rtl_lr(&t0, R_AX, 2);
-		rtl_sext(&t0, &t0, 2);
-		rtl_sr(R_EAX, &t0, 4);
+		rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
 	}
-
+	
+	print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
+}
+// Convert Signed word to Signed Long
+make_EHelper(cwtl) {
+	if (decoding.is_operand_size_16) {
+		rtl_shli(&reg_l(R_EAX), &reg_l(R_EAX), 24);
+		rtl_sari(&reg_l(R_EAX), &reg_l(R_EAX), 8);
+	rtl_shri(&reg_l(R_EAX), &reg_l(R_EAX), 16);
+	}
+	else {
+		rtl_sext(&reg_l(R_EAX), &reg_l(R_EAX), 2);
+	} 
 	print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
