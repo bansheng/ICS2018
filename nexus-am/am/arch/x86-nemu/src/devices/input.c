d1f542d0626fcd7541cbbab790f9c2e9d1c952e9
tracer-ics2018 <tracer@njuics.org>
2018-12-17 18:19:52 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:19:52 up 4 days, 12:36,  7 users,  load average: 0.20, 0.38, 0.26 f7d4352355866b35825145425bda384750757dfa

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -1,15 +1,26 @@
 #include <am.h>
 #include <x86.h>
 #include <amdev.h>
+#include <klib.h>
+#define I8042_DATA_PORT 0x60 //默认键盘端口
 
+unsigned long long press;
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      //press = 1;
+      //printf("key1=%d\n",(uint32_t)press);
+      press = inl(I8042_DATA_PORT);
+      //printf("key2=%d\n",(uint32_t)press);
+      kbd->keycode = press;
+      if(press != _KEY_NONE)
+         kbd->keydown = 1;
+      else
+         kbd->keydown = 0;
       return sizeof(_KbdReg);
     }
   }
   return 0;
 }
+
