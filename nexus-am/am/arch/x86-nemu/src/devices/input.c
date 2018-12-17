4541cb22d5b5a4047b2c59f1feab4642232e32b1
tracer-ics2018 <tracer@njuics.org>
2018-12-17 18:28:01 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:28:01 up 4 days, 12:45,  7 users,  load average: 0.44, 0.44, 0.32 447a8d6cb27409b1a85c932f791c5ae752627757

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -6,19 +6,20 @@
 
 unsigned long long press;
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_INPUT_KBD: {
-      _KbdReg *kbd = (_KbdReg *)buf;
-      //press = 1;
-      //printf("key1=%d\n",(uint32_t)press);
-      press = inl(I8042_DATA_PORT);
-      //printf("key2=%d\n",(uint32_t)press);
-      kbd->keycode = press;
-      kbd->keydown = !(kbd->keydown);
-      printf("keydown=%d\n",kbd->keydown);
-      return sizeof(_KbdReg);
-    }
-  }
-  return 0;
+	switch (reg) {
+		case _DEVREG_INPUT_KBD: {
+			_KbdReg *kbd = (_KbdReg *)buf;
+			//press = 1;
+			//printf("key1=%d\n",(uint32_t)press);
+			press = inl(I8042_DATA_PORT);
+			//printf("key2=%d\n",(uint32_t)press);
+			kbd->keycode = press;
+			if(press != _KEY_NONE)
+				kbd->keydown = !(kbd->keydown);
+			// printf("keydown=%d\n",kbd->keydown);
+			return sizeof(_KbdReg);
+		}
+	}
+	return 0;
 }
 
