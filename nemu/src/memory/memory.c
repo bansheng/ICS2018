0f03cd6658bf03fb734b69f8c4bec2cb4098ac15
tracer-ics2018 <tracer@njuics.org>
2018-12-17 18:46:24 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:46:24 up 4 days, 13:03,  7 users,  load average: 0.10, 0.10, 0.16 ba7c662f6d0f4dff971ad4efca677dac8edd79b8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,19 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+	int mmio_n;
+	if ((mmio_n = is_mmio(addr)) != -1)
+		return mmio_read(addr, len, mmio_n);
+	else
+		return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
-  memcpy(guest_to_host(addr), &data, len);
+	int mmio_n;
+	if ((mmio_n = is_mmio(addr)) != -1)
+		mmio_write(addr, len, data, mmio_n);
+	else
+ 		memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -2,15 +2,18 @@
 #include <x86.h>
 #include <amdev.h>
 #include <klib.h>
-
+#define SCREEN_PORT 0x100
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 0;
-      info->height = 0;
+      uint32_t screen;
+      screen = inl(SCREEN_PORT);
+      //printf("%d\n",screen);
+      info->width = screen >> 16;
+      info->height = screen << 16 >> 16;
       return sizeof(_VideoInfoReg);
     }
   }
@@ -18,18 +21,21 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 size_t video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _FBCtlReg *ctl = (_FBCtlReg *)buf;
-
-      if (ctl->sync) {
-        // do nothing, hardware syncs.
-      }
-      return sizeof(_FBCtlReg);
-    }
-  }
-  return 0;
+	switch (reg) {
+		case _DEVREG_VIDEO_FBCTL: {
+			_FBCtlReg *ctl = (_FBCtlReg *)buf;
+			int i;
+			int size = screen_width() * screen_height();
+			for (i = 0; i < size; i ++) fb[i] = i;
+			if (ctl->sync) {
+			// do nothing, hardware syncs.
+			}
+			return sizeof(_FBCtlReg);
+		}
+	}
+	return 0;
 }
 
 void vga_init() {
 }
+
