9c949525b35777e72bbfed1a98d097d892fc7771
tracer-ics2018 <tracer@njuics.org>
2018-12-20 11:42:37 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:42:37 up 10:30,  7 users,  load average: 0.32, 0.22, 0.18 879ba209fb93683fd6437199dc17e919ee517b67

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -35,6 +35,7 @@ make_EHelper(mov_cr2r) {
 
 make_EHelper(int) {
 	// TODO();
+	printf("irq %d\n", id_dest->val);
 	raise_intr(id_dest->val, decoding.seq_eip);
 
 	print_asm("int %s", id_dest->str);
