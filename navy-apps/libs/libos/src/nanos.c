2e73df570d98417b005e19ef7c6da7c1b81fc623
tracer-ics2018 <tracer@njuics.org>
2018-12-20 18:40:33 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:40:33 up 16:46,  7 users,  load average: 0.33, 0.27, 0.15 10da8642973138bebeff2fff132e534db1dcce12

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -40,10 +40,10 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-	char ch = 's';
-	_putc(&ch);
+	panic("11111");
 	intptr_t old_pb = program_break;
 	if (_syscall_(SYS_brk, old_pb + increment, 0, 0) == 0) {
+		panic("222");
 		program_break += increment;	
 		return (void *)old_pb;
 	}
