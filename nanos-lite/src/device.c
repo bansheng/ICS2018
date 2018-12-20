28a5b0cae4d223d77e96b2c11c71eb8fddf473c9
tracer-ics2018 <tracer@njuics.org>
2018-12-20 23:08:23 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:08:23 up 21:14,  6 users,  load average: 0.54, 0.50, 0.33 61065b411213acf1395c80b542c728c9d21e0a6c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,35 +2,83 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	uintptr_t i = 0;
+	for(; len > 0; len--) {
+		_putc(((char*)buf)[i]);
+		i++;;
+	}
+	return i;
 }
 
 #define NAME(key) \
   [_KEY_##key] = #key,
 
 static const char *keyname[256] __attribute__((used)) = {
-  [_KEY_NONE] = "NONE",
-  _KEYS(NAME)
+	[_KEY_NONE] = "NONE",
+	_KEYS(NAME)
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+	int key = read_key();
+	bool down = false;
+	if (key & 0x8000) {
+		key ^= 0x8000;
+		down = true;
+	}
+	if (key == _KEY_NONE) {
+		unsigned long t = uptime();
+		sprintf(buf, "time %d\n", t);
+	}
+	else {
+		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+	}
+	return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+	strncpy(buf, dispinfo + offset, len);
+	return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+	int x, y;
+	int len1, len2, len3;
+	//offset /= 4;
+	offset = offset >> 2;
+	y = offset / screen_width();
+	x = offset % screen_width();
+	
+  //Log("fb_write x:%d y:%d len:%d\n", x, y, len);
+	//len /= 4;
+	len = len >> 2;
+	len1 = len2 = len3 = 0;
+	
+	// the first line
+	len1 = len <= screen_width() - x ? len : screen_width() - x;
+	draw_rect((uint32_t *)buf, x, y, len1, 1);
+
+	// the middle line
+	if (len > len1 && ((len - len1) > screen_width()))	{
+		//len2 = (len - len1) / screen_width() * screen_width();
+		len2 = len - len1;
+		draw_rect((uint32_t *)buf + len1, 0, y + 1, screen_width(), len2 / screen_width());
+	}	
+
+	// the lase line
+	if (len - len1 - len2 > 0) {
+		len3 = len - len1 - len2;
+		draw_rect((uint32_t *)buf + len1 + len2, 0, y + len2 / screen_width() + 1, len3, 1);
+	}
+	return len;
 }
 
 void init_device() {
-  Log("Initializing devices...");
-  _ioe_init();
+	Log("Initializing devices...");
+	_ioe_init();
 
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
+	// TODO: print the string to array `dispinfo` with the format
+	// described in the Navy-apps convention
+	strcpy(dispinfo ,"WIDTH:640\nHEIGHT:480\n");
 }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,45 +1,53 @@
 #include "fs.h"
 
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-extern size_t get_ramdisk_size();
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t serial_write(const void *buf, size_t offset, size_t len);
+
+extern size_t events_read(void *buf, size_t offset, size_t len);
+extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
+extern size_t fb_write(const void *buf, size_t offset, size_t len);
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
 typedef struct {
-  char *name;
-  size_t size;
-  size_t disk_offset;
-  ReadFn read;
-  WriteFn write;
-  size_t open_offset; //读写指针,这个是后来按照要求添加的
+	char *name;
+	size_t size;
+	size_t disk_offset;
+	size_t open_offset; //读写指针,这个是后来按照要求添加的
+	ReadFn read;
+	WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
+	panic("should not reach here");
+	return 0;
 }
 
 size_t invalid_write(const void *buf, size_t offset, size_t len) {
-  panic("should not reach here");
-  return 0;
+	panic("should not reach here");
+	return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+  {"stdin", 0, 0, 0,invalid_read, invalid_write},
+  {"stdout", 0, 0, 0, invalid_read, serial_write},
+  {"stderr", 0, 0, 0, invalid_read, serial_write},
+  [FD_FB] = {"/dev/fb", 0, 0, 0, NULL, fb_write},
+  [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read},
+  [FD_DISPINFO] = {"/proc/dispinfo", 128, 0, 0, dispinfo_read},
 #include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
 void init_fs() {
-  // TODO: initialize the size of /dev/fb
+	// TODO: initialize the size of /dev/fb
+	file_table[FD_FB].size = screen_width() * screen_height() * 4;  
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
@@ -58,55 +66,60 @@ int fs_open(const char *pathname, int flags, int mode) {
 	return -1;
 }
 
-size_t fs_read(int fd, void *buf, size_t len)
-{
-	
-	if(fd > 2)
-	{
-		if(file_table[fd].open_offset >=  fs_filesz(fd)|| len == 0)
-		{
-			Log("file read hit the End!");
-			return 0;
-		}
-				
-		if(file_table[fd].open_offset + len > fs_filesz(fd))
-			len = fs_filesz(fd) - file_table[fd].open_offset;
-		ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-		file_table[fd].open_offset += len;
+size_t fs_read(int fd, void *buf, size_t len) {
+	size_t fs_size = fs_filesz(fd);
+	//Log("in the read, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, fs_size, len, file_table[fd].open_offset);
+	switch(fd) {
+		case FD_STDIN:
+		case FD_STDOUT:
+		case FD_FB:
+			break;
+		case FD_EVENTS:
+			//Log("in the fs_read fd_fb\n");
+			len = file_table[fd].read(buf, 0, len);
+			break;
+		case FD_DISPINFO:
+			if (file_table[fd].open_offset >= file_table[fd].size)
+				return 0;
+			if (file_table[fd].open_offset + len > file_table[fd].size)
+				len = file_table[fd].size - file_table[fd].open_offset;
+			file_table[fd].read(buf, file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;	
+			break;
+		default:
+			if(file_table[fd].open_offset >= fs_size || len == 0)
+				return 0;
+			if(file_table[fd].open_offset + len > fs_size)
+				len = fs_size - file_table[fd].open_offset;
+			ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+			break;
 	}
 	return len;
 }
 
-uintptr_t sys_write(int fd, const void *buf, size_t len) {
-	// Log(" 1 ");
-	uintptr_t i = 0;
-	if (fd == 1 || fd == 2) {
-		for(; len > 0; len--) {
-			_putc(((char*)buf)[i]);
-			i++;;
-		}
+size_t fs_write(int fd, const void *buf, size_t len) {
+	size_t fs_size = fs_filesz(fd);
+	//Log("in the write, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, fs_size, len, file_table[fd].open_offset);
+	switch(fd) {
+		case FD_STDIN: break;
+		case FD_STDOUT:
+		case FD_STDERR:
+			file_table[fd].write(buf, 0, len);
+			break;
+		case FD_FB:
+			file_table[fd].write(buf, 0, len);
+			break;
+		default:
+			// write to ramdisk
+			if(file_table[fd].open_offset >= fs_size)
+				return 0;	
+			if(file_table[fd].open_offset + len > fs_size)
+				len = fs_size - file_table[fd].open_offset;
+			ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+			break;
 	}
-	return i;
-}
-
-size_t fs_write(int fd, const void *buf, size_t len)
-{
-	if(fd == 1 || fd == 2) return sys_write(fd, buf, len);
-	else if(fd > 2)
-	{
-		if(file_table[fd].open_offset >= fs_filesz(fd) || len == 0)
-		{
-			Log("file write hit the End!");
-			return 0;
-		}
-				
-		if(file_table[fd].open_offset + len > fs_filesz(fd))
-			len = fs_filesz(fd) - file_table[fd].open_offset;
-		ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-		file_table[fd].open_offset += len;
-		return len;
-	}
-	
 	return len;
 }
 
