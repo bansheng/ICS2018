d596fa3f62cca74a395f76911450d8a02f6d0aa1
tracer-ics2018 <tracer@njuics.org>
2018-12-20 12:43:10 +0800

    >  compile U201514658 dyd Linux ubuntu 4.4.0-140-generic #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  12:43:10 up 10:48,  7 users,  load average: 0.06, 0.14, 0.16 63b8b9cceb31071c73fa525fab94ac0239ba30c

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -13,7 +13,6 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 	} item;
 	vaddr_t addr;
 	
-	printf("%x\n", cpu.idtr.base);
 	addr = 8 * NO + cpu.idtr.base;
 	item.lo = vaddr_read(addr, 4);
 	item.hi = vaddr_read(addr + 4, 4);
