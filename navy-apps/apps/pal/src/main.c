08983c834022e03a8c07f58b834e870460f92162
tracer-ics2018 <tracer@njuics.org>
2018-12-22 10:21:48 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:21:48 up 12:10,  9 users,  load average: 0.78, 0.60, 0.37 d5d073dde8d4fc2e55e650ee133a64b2457f77f

--- navy-apps/apps/pal/src/main.c
+++ navy-apps/apps/pal/src/main.c
@@ -231,9 +231,13 @@ PAL_TrademarkScreen(
 
 --*/
 {
+   Log("11111");
    PAL_SetPalette(3, FALSE);
+   Log("11111");
    PAL_RNGPlay(6, 0, 1000, 25);
+   Log("11111");
    UTIL_Delay(1000);
+   Log("11111");
    PAL_FadeOut(1);
    Log("11111");
 }
