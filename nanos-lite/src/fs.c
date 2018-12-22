56f54ff5cc4b9d95a958d4f928f5fe43ca8bcde0
tracer-ics2018 <tracer@njuics.org>
2018-12-22 18:59:52 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:59:52 up 17:29,  9 users,  load average: 0.62, 0.41, 0.16 ccebeddef127085beb2a92671637832fb9438123

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,7 +76,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 	size_t fs_size = fs_filesz(fd);
 	
 	
-	Log("in the read, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, fs_size, len, file_table[fd].open_offset);
+	//Log("in the read, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, fs_size, len, file_table[fd].open_offset);
 	switch(fd) {
 		case FD_STDIN:
 		case FD_STDOUT:
