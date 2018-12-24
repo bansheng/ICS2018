ab9c75533d6f11bfc96f1897e9ec79b039e9243a
tracer-ics2018 <tracer@njuics.org>
2018-12-24 09:33:25 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:33:25 up  1:11,  5 users,  load average: 0.29, 0.15, 0.11 152ea5f6f860089d343b130c783e01949f747336

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -68,11 +68,11 @@ _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
 /*	uintptr_t error_code, eip, cs, eflags; */
 /*	};*/
 /*	_Context* ct = (_Context*)((uintptr_t)stack + STACK_SIZE - sizeof(_Context));*/
-	_Context* ct = (stack.end - sizeof(_Context) - sizeof(void*)); //包括esp和一个protect*
+	_Context* ct = (stack.end - sizeof(_Context)); //包括esp和一个protect*
 	// printf("ct = %X  end = %X\n", (uintptr_t)ct, (uintptr_t)stack.end);
 	memset(ct, 0, sizeof(_Context));
-	*(uintptr_t *)(stack.end -sizeof(uintptr_t*))  = (uintptr_t)entry; //设置返回值
-	//printf("address=%X value=%X", stack.end -sizeof(uintptr_t*), *(uintptr_t *)(stack.end -sizeof(uintptr_t*)));
+	ct->eip  = (uintptr_t)entry; //设置返回值
+	//printf("address=%X value=%X", stack.end, *(uintptr_t *)(stack.end));
 	ct->cs = 8;
 	// ct->irq = 0x81; //yeild
 	return ct;
