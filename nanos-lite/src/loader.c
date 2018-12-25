a7e9a8a94f3764a497b47a2ce6962d9cd0592bbe
tracer-ics2018 <tracer@njuics.org>
2018-12-26 00:15:06 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:15:06 up 1 day,  6:13,  5 users,  load average: 0.32, 0.40, 0.27 3d346ce6149c8b15e63fc637936372eedeea121

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -44,8 +44,8 @@ void context_kload(PCB *pcb, void *entry) {
 }
 
 void context_uload(PCB *pcb, const char *filename) {
-	_protect(&(pcb->as)); //创建m默认地址空间
-	printf("pcb0 地址空间%X\n", (uintptr_t)((pcb->as).ptr));
+	_protect(&(pcb->as)); //创建默认地址空间
+	// printf("pcb0 地址空间%X\n", (uintptr_t)((pcb->as).ptr));
 	uintptr_t entry = loader(pcb, filename);
 	_Area stack;
 	stack.start = pcb->stack;
--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -4,5 +4,6 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-	return _syscall_(SYS_yield, 0, 0, 0);
+/*	return _syscall_(SYS_yield, 0, 0, 0);*/
+	return 0;
 }
