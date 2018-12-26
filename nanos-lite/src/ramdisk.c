bbc5fa8bbe2eecc0a7c0ddd5ad939cbcc8b1539b
tracer-ics2018 <tracer@njuics.org>
2018-12-26 17:50:25 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:50:25 up  8:41,  5 users,  load average: 0.00, 0.02, 0.07 bb8e4ce1e9f0f28bca040edda70d0955c25b435

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -10,7 +10,8 @@ extern uint8_t ramdisk_end;
  */
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
-size_t ramdisk_read(void *buf, size_t offset, size_t len) {
+size_t ramdisk_read(void *buf, size_t offset, size_t len) {\
+	printf("ramdisk_read %X\n", buf);
 	assert(offset + len <= RAMDISK_SIZE);
 	memcpy(buf, &ramdisk_start + offset, len);
 	return len;
