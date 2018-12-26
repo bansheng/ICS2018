4afc5c996f7778e4bcbf3571bc2d6d9233cb2869
tracer-ics2018 <tracer@njuics.org>
2018-12-26 11:39:51 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:39:51 up  3:13,  5 users,  load average: 0.15, 0.06, 0.01 3f83c82993371cfc78f22d80ef5b76d8aedde471

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,15 +16,19 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
+	printf("new_brk = %X\n", new_brk);
 	printf("current->cur_brk = %X\n", current->cur_brk);
 	if (current->cur_brk == 0) {
 		current->cur_brk = current->max_brk = new_brk;
-/*		current->cur_brk = current->max_brk = 0;*/
+/*		for (va = (current->max_brk+0xfff) & ~0xfff; va < new_brk; va += PGSIZE) { //取低12位*/
+/*			pa = (uintptr_t)new_page(1);*/
+/*			_map(&current->as, (void *)va, (void *)pa, 1);*/
+/*		}*/
 	} 
 	else {
 		if (new_brk > current->max_brk) {
 			uintptr_t pa, va;
-			printf("va = 0x%X", (current->max_brk+0xfff) & ~0xfff);
+			printf("va = 0x%X\n", (current->max_brk+0xfff) & ~0xfff);
 			for (va = (current->max_brk+0xfff) & ~0xfff; va < new_brk; va += PGSIZE) { //取低12位
 				pa = (uintptr_t)new_page(1);
 				_map(&current->as, (void *)va, (void *)pa, 1);
