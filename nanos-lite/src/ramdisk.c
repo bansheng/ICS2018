dca57ae917c30df530608cdec666ce895d81b956
tracer-ics2018 <tracer@njuics.org>
2018-12-26 18:06:21 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:06:21 up  8:57,  5 users,  load average: 0.45, 0.15, 0.10 de59424f5264548c8345915ee9f13651dc768ae0

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -25,10 +25,10 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
 }
 
 void init_ramdisk() {
-  Log("ramdisk info: start = %p, end = %p, size = %d bytes",
-      &ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
+	Log("ramdisk info: start = %p, end = %p, size = %d bytes",
+    	&ramdisk_start, &ramdisk_end, RAMDISK_SIZE);
 }
 
 size_t get_ramdisk_size() {
-  return RAMDISK_SIZE;
+	return RAMDISK_SIZE;
 }
