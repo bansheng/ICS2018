0d73b6f3b151be4a8b5ecba63f01383f2754c298
tracer-ics2018 <tracer@njuics.org>
2018-12-26 16:43:13 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:43:13 up  7:34,  5 users,  load average: 0.06, 0.06, 0.02 aada94763544a30575459c5c88b1988367c74ac2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc() {
 /*	*/
 	extern void context_uload(PCB *pcb, const char *filename);
 	context_uload(&pcb[0], "/bin/text");
-	context_uload(&pcb[1], "/bin/hello");
+/*	context_uload(&pcb[1], "/bin/hello");*/
 /*	*/
  	switch_boot_pcb();
 /* 	printf("init proc over\n");*/
@@ -39,8 +39,8 @@ _Context* schedule(_Context *prev) {
 	current->cp = prev;
 
 	// always select pcb[0] as the new process
-/*	current = &pcb[0];*/
-	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+	current = &pcb[0];
+/*	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);*/
 
 	// then return the new context
 	return current->cp;
