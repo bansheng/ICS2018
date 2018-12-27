ad038e96803baaefcc15931fdbdf60011cc57b6e
tracer-ics2018 <tracer@njuics.org>
2018-12-27 10:36:20 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:36:20 up 13:17,  5 users,  load average: 0.12, 0.22, 0.29 50f647fe134f24583a4d35c07ad3aed639fb5c3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,88 +1,54 @@
 #include "proc.h"
+#include "memory.h"
 #include "fs.h"
+#define DEFAULT_ENTRY ((void *)0x8048000)
 
-#ifdef HAS_VME
-#define DEFAULT_ENTRY 0x8048000
-#else
-#define DEFAULT_ENTRY 0x4000000
-#endif
-//void _switch_addr(_Protect *p);
-void _switch(_Context *c);
-void get_cur_as(_Context *c);
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  int file = fs_open(filename,0,0);
-  if(file==-1)
-    panic("File does not exist!\n");
-  #ifdef HAS_VME
-  uint32_t progSize = fs_filesz(file);
-  uint32_t pgCnt = progSize / (pcb->as.pgsize)+((progSize%(pcb->as.pgsize))!=0);
-  /*uint32_t tblCnt = pgCnt / 1024 +((pgCnt%1024)!=0);
-  for(int i = 0; i < tblCnt; i++){
-    void* npgtbl = new_page(1);
-        //printf("Attempted to create page table for VA:%d at PA:%d\n",DEFAULT_ENTRY+(i<<20),
-          //(uintptr_t) npgtbl);
-    _map(&pcb->as,(void*)(DEFAULT_ENTRY+(i<<22)),npgtbl,1);
-  }*/
-  for(int i = 0; i < pgCnt; i++){
-    void* npg = new_page(1);
-    _map(&pcb->as,(void*)(long)(DEFAULT_ENTRY+(i<<12)),npg,0);
-  }
-  _Context fake;
-  fake.prot = &pcb->as;
-  _switch(&fake);
-  #endif
-  
-  fs_read(file,(void*)(long)DEFAULT_ENTRY,fs_filesz(file));
-  return DEFAULT_ENTRY;
-
+	// TODO();
+	_Protect *as = &(pcb->as);
+	int fd = fs_open(filename, 0, 0);
+	size_t nbyte = fs_filesz(fd);
+	void *pa;
+	void *va;
+	void *end = DEFAULT_ENTRY + nbyte;
+	
+	printf("loaded: [%d]%s size:%d start=%X, end=%X\n", fd, filename, nbyte, DEFAULT_ENTRY, end);
+	
+	for (va = (void *)DEFAULT_ENTRY; va < end; va += PGSIZE) {
+		pa = new_page(1); //new_page(1) 和 _protect 的区别在于_protect里面还是有默认的内核地址映射的
+		// _map(_Protect *p, void *va, void *pa, int prot)
+		// printf("va = %X => pa = %X\n", va, pa);
+		_map(as, va, pa, 1);
+		fs_read(fd, pa, (end - va) < PGSIZE ? (end - va) : PGSIZE);
+	}
+	
+/*	int fd = fs_open(filename, 0, 0);*/
+/*	fs_read(fd, (void *)DEFAULT_ENTRY, fs_filesz(fd));*/
+/*	fs_close(fd);*/
+	
+	return (uintptr_t)DEFAULT_ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  ((void(*)())entry) ();
+	uintptr_t entry = loader(pcb, filename);
+	((void(*)())entry) ();
 }
 
 void context_kload(PCB *pcb, void *entry) {
-#ifdef HAS_VME
-  _protect(&(pcb->as));
-#endif
-  _Area stack;
-  stack.start = pcb->stack;
-  stack.end = stack.start + sizeof(pcb->stack);
+	_Area stack;
+	stack.start = pcb->stack;
+	stack.end = stack.start + sizeof(pcb->stack);
 
-  pcb->cp = _kcontext(stack, entry, NULL);
-  pcb->cp->prot = &(pcb->as);
+	pcb->cp = _kcontext(stack, entry, NULL);
 }
 
-#ifdef HAS_VME
-  static _Context old;
-  static _Protect m;
-#endif
-
 void context_uload(PCB *pcb, const char *filename) {
-  #ifdef HAS_VME
-  get_cur_as(&old);
-  if(old.prot==NULL)
-    {
-      _protect(&(pcb->as));
-      m = pcb->as;
-    }
-  else
-    {
-      m = *(old.prot);
-      _protect(&(pcb->as));
-    }
-  #endif
-  uintptr_t entry = loader(pcb, filename);
-  old.prot = &m;
-  _switch(&old);
+	_protect(&(pcb->as)); //创建默认地址空间
+	printf("地址空间%X\n", (uintptr_t)((pcb->as).ptr));
+	uintptr_t entry = loader(pcb, filename);
+	_Area stack;
+	stack.start = pcb->stack;
+	stack.end = stack.start + sizeof(pcb->stack);
 
-  _Area stack;
-  stack.start = pcb->stack;
-  stack.end = stack.start + sizeof(pcb->stack);
-
-  pcb->cp = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
-  int file = fs_open(filename,0,0);
-  pcb->cur_brk = DEFAULT_ENTRY+fs_filesz(file);
-  pcb->max_brk = pcb->cur_brk;
+	pcb->cp = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
 }
