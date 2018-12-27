076489db7a1cb1f4f771564cf17f941a3f4e49ed
tracer-ics2018 <tracer@njuics.org>
2018-12-27 10:12:46 +0800

    >  run U201514658 dyd Linux ubuntu 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:12:46 up 12:54,  5 users,  load average: 0.37, 0.40, 0.41 61da0bcf2a7aaccbb654118d5306ec34861ddf0

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,8 +39,8 @@ _Context* schedule(_Context *prev) {
 	current->cp = prev;
 
 	// always select pcb[0] as the new process
-/*	current = &pcb[0];*/
-	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+    current = &pcb[0];
+	// current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
 	// then return the new context
 	return current->cp;
